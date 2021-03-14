e34fc2a481d8d5f5f3def9dd14076d20ced2708a
sergio <sdlbsso@gmail.com>
2021-03-14 04:06:12 -0300

    proyecto api con firebase

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+
+
+const app = express();
+
+//settings
+
+
+
+
+//midlewares
+
+
+//rutas
+
+
+
+//archivos estaticos
+
+
+
+
+
+module.exports = app;
