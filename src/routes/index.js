c1118ed24772547005979901eb1ffd566eb7f847
sergio <sdlbsso@gmail.com>
2021-03-15 00:53:36 -0300

    listar agregar y borrar de la base de datos firebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,42 @@
 const { Router } = require('express');
 const router = Router();
+const admin = require('firebase-admin');
+
+var serviceAccount = require("../../node-firebase-a4504-firebase-adminsdk-vg9f3-1dec852965.json")
+
+admin.initializeApp({  
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://node-firebase-a4504-default-rtdb.firebaseio.com/'
+});
+
+const db = admin.database();
 
 router.get('/',(req, res)=>{
-    res.render('index');
+    //primero se consulta bd para listar
+    db.ref('contactos').once('value',(snapshot)=>{
+       const data = snapshot.val();
+       res.render('index',{ contactos : data });
+    });
 });
+
+//new-contact
+router.post('/new-contact',(req,res)=>{
+    const newContact = {
+        firstname : req.body.firstname,
+        lastname : req.body.lastname,
+        email : req.body.email,
+        phone : req.body.phone
+    };
+    //console.log(newContact);
+    db.ref('contactos').push(newContact);
+    res.redirect('/');
+});
+
+///delete/
+router.get('/delete/:id',(req,res)=>{
+    db.ref('contactos/'+req.params.id).remove();
+    res.redirect('/');
+});
+
 
 module.exports = router;
